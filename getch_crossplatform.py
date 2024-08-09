4a2f3b9f608a4a6064470874a6957bb853f7c0f1
TheHerowither <lucland278@gmail.com>
2024-08-10 00:31:40 +0200

    Fix getch_crossplatform

--- getch_crossplatform.py
+++ getch_crossplatform.py
@@ -1,38 +1,28 @@
-class _Getch:
-    """Gets a single character from standard input.  Does not echo to the
-screen."""
-    def __init__(self):
-        try:
-            self.impl = _GetchWindows()
-        except ImportError:
-            self.impl = _GetchUnix()
-
-    def __call__(self): return self.impl()
+import sys
 
 
-class _GetchUnix:
-    def __init__(self):
-        import tty, sys
-
-    def __call__(self):
-        import sys, tty, termios
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
+def _Getch() -> str:
+    if sys.platform == "win32":
+       return _GetchWindows()
+    else:
+        return _GetchUnix()
 
 
-class _GetchWindows:
-    def __init__(self):
-        import msvcrt
+def _GetchUnix() -> str:
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+       termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
 
-    def __call__(self):
-        import msvcrt
-        return msvcrt.getch()
+
+def _GetchWindows():
+    import msvcrt
+    return msvcrt.getch()
 
 
 getch = _Getch
