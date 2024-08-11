534fb6e3046fbd8fbf873401e7e63efb0694024c
TheHerowither <lucland278@gmail.com>
2024-08-11 14:33:18 +0200

    Add backspace to writer

--- writing.py
+++ writing.py
@@ -10,11 +10,16 @@ class TypingBuffer:
         self.cursor_pos : int = 0;
 
     def update(self):
-        print(self.buffer+"\033[1;30m"+self.target_text[self.cursor_pos:]+"\033[0m")
+        print(self.buffer+"\033[1;30m"+self.target_text[self.cursor_pos:]+"\033[0m", end = "\r")
 
     def add_char(self, c : str):
         self.buffer += c
         self.cursor_pos += 1
+
+    def remove_char(self):
+        if self.cursor_pos > 0:
+            self.buffer = self.buffer[:-1]
+            self.cursor_pos -= 1
 
 
 if __name__ == "__main__":
@@ -23,4 +28,5 @@ if __name__ == "__main__":
     while True:
         buf.update()
         c = getch()
-        buf.add_char(c)
+        if c == '\x7f': buf.remove_char()
+        else: buf.add_char(c)
