a635508cefe0b2887c08dd11582c4efffc07a4f5
TheHerowither <lucland278@gmail.com>
2024-08-12 01:23:59 +0200

    Almost implement minesweeper

--- minesweeper.py
+++ minesweeper.py
@@ -3,9 +3,13 @@ import random
 import os
 import subprocess
 import sys
+import time
 
 
 # Created by TheHerowither, based on (https://en.wikipedia.org/wiki/Minesweeper_(video_game))
+# TODO: Win condition
+# TODO: Point system
+
 
 class GridPos:
     def __init__(self, x : int | float, y : int | float):
@@ -16,20 +20,24 @@ class MinesweeperSquare:
     def __init__(self, value : int = 0) -> None:
         self.opened = False
         self.value = value
-        self.is_bomb = False    # redundant field [Pickle]
+        self.flag = False
 
 
 def print_square(square : MinesweeperSquare, end : str = "\n", is_selected : bool = False):
     if square.opened:
-        if square.is_bomb:
-            if is_selected: print(f"\033[47m\033[30m▣\033[0m", end = end)
+        if square.value == -1:
+            if is_selected: print("\033[47m\033[30m▣\033[0m", end = end)
             else: print("▣", end = end)
         else:
             if is_selected: print(f"\033[47m\033[30m{square.value}\033[0m", end = end)
             else: print(square.value, end = end)
     else:
-        if is_selected: print(f"\033[47m\033[30m■\033[0m", end = end)
-        else: print("■", end = end)
+        if square.flag:
+            if is_selected: print("\033[47m\033[30m⚑\033[0m", end = end)
+            else: print("⚑", end = end)
+        else:
+            if is_selected: print(f"\033[47m\033[30m■\033[0m", end = end)
+            else: print("■", end = end)
 
 class Minesweeper:
     def __init__(self, size : str | tuple[int, int] | GridPos = "10x10") -> None:
@@ -51,28 +59,68 @@ class Minesweeper:
         for x in range(0, self.size.x * self.size.y):
             self.board.append(MinesweeperSquare(0))
 
-        bombs = 0
         for i in range(0, num_bombs):
             idx = random.randint(0, len(self.board)-1)
-            while not self.board[idx].is_bomb:
-                if not self.board[idx].is_bomb:
-                    self.board[idx].is_bomb = True
-                    bombs += 1
+            while self.board[idx].value != -1:
+                if self.board[idx].value != -1:
+                    self.board[idx].value = -1
                     break
                 idx = random.randint(0, len(self.board)-1)
 
+        for x in range(0, self.size.x):
+            for y in range(0, self.size.y):
+                # This is a very dirty solution!
+                # TODO: Find a better way of doing this
+                if self.board[self.get_idx(x, y)].value != -1:
+                    bombs = 0
+                    
+                    if y >> 0:
+                        if self.board[self.get_idx(x,   y-1)].value == -1: bombs += 1
+                        if self.board[self.get_idx(x+1, y-1)].value == -1: bombs += 1
+
+                    if x >> 0:
+                        if self.board[self.get_idx(x-1, y+1)].value == -1: bombs += 1
+                        if self.board[self.get_idx(x-1, y)].value == -1: bombs += 1
+    
+                    if (x >> 0 and y >> 0):
+                        if self.board[self.get_idx(x-1, y-1)].value == -1: bombs += 1
+
+                    if self.board[self.get_idx(x,   y)].value == -1: bombs += 1
+                    if self.board[self.get_idx(x+1, y)].value == -1: bombs += 1
+
+                    if self.board[self.get_idx(x,   y+1)].value == -1: bombs += 1
+                    if self.board[self.get_idx(x+1, y+1)].value == -1: bombs += 1
+
+
+                    self.board[self.get_idx(x, y)].value = bombs
+
+                    
+
+                
+    def get_idx(self, x : int, y : int) -> int:
+        return min(y, self.size.y - 1) * self.size.y + min(x, self.size.x - 1)
+
     def update(self):
+        subprocess.call('clear' if os.name == 'posix' else 'cls')
         for square_idx in range(1, len(self.board)+1):
             idx = self.cursor.y * self.size.y + self.cursor.x
             if square_idx % self.size.x == 0: print_square(self.board[square_idx-1], is_selected=square_idx-1 == idx)
             else: print_square(self.board[square_idx-1], end = "", is_selected=square_idx-1 == idx)
 
+
+
+    def uncover_all(self):
+        for i in range(0, len(self.board)):
+            self.board[i].opened = True
+            time.sleep(0.01)
+            self.update()
+
     def run(self):
         running = True
         while running:
             try:
-                subprocess.call('clear' if os.name == 'posix' else 'cls')
                 self.update()
+                print(f"Cursor: {self.cursor.x}, {self.cursor.y}")
                 c = getch()
                 if c == '\033':
                     getch()
@@ -88,13 +136,20 @@ class Minesweeper:
                             if self.cursor.x > 0: self.cursor.x -= 1
                 elif (c == '\n' or c == '\n\r' or c == '\r'):
                     self.board[self.cursor.y * self.size.y + self.cursor.x].opened = True
+                    if self.board[self.get_idx(self.cursor.x, self.cursor.y)].value == -1:
+                        self.uncover_all()
+                        self.update()
+                        print("GAME OVER!!")
+                        running = False;
+                elif c == 'f': 
+                    self.board[self.get_idx(self.cursor.x, self.cursor.y)].flag = True
             except KeyboardInterrupt:
                 sys.exit(0)
 
 
 
 if __name__ == "__main__":
-    game = Minesweeper("9x9")
+    game = Minesweeper("15x15")
     game.generate_board(10)
 
     game.run()
